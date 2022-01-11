b2655285291c5cd118a5fdaf8fbf81b99a86c07e
junaidapdev <junaidap.dev.com>
2022-01-11 22:01:12 +0530

    name section added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        I'm Mohammed <br/>
+        Junaid
+      </SectionTitle>
+      <SectionText>
+        Frontend Developer who loves to build products and make peoples task easy.
+      </SectionText>
+      <Button onclick={() => window.location = "https://twitter.com/mdjunaidap"}>Contact Me </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
